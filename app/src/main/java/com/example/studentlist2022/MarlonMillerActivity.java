f59cbc967c1aa3a206b72063852a3470b344dc2f
Marlon Miller <mem19b@acu.edu>
2022-09-08 08:30:39 -0500

    New ID "App_MEMM" for my ListView in my own activity

--- app/src/main/java/com/example/studentlist2022/MarlonMillerActivity.java
+++ app/src/main/java/com/example/studentlist2022/MarlonMillerActivity.java
@@ -3,12 +3,28 @@ package com.example.studentlist2022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MarlonMillerActivity extends AppCompatActivity {
+
+    ListView languageList;
+
+    // Array of strings...
+    String langArray[] = {
+            "Risky World Accounting",
+            "ACU Greek Life",
+            "Money Handler",
+            "The Social Meme",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marlon_miller);
+
+        languageList = (ListView)findViewById(R.id.App_MEMM);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, langArray);
+        languageList.setAdapter(arrayAdapter);
     }
 }
