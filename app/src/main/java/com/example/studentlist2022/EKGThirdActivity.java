ba0b220f1dba19a5ba27883841607bb91cd51172
Evan Grau <89275830+whatissatire@users.noreply.github.com>
2022-09-08 14:54:27 -0500

    working on third activity

--- app/src/main/java/com/example/studentlist2022/EKGThirdActivity.java
+++ app/src/main/java/com/example/studentlist2022/EKGThirdActivity.java
@@ -2,7 +2,12 @@ package com.example.studentlist2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 public class EKGThirdActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/studentlist2022/EvanGrauActivity.java
+++ app/src/main/java/com/example/studentlist2022/EvanGrauActivity.java
@@ -2,13 +2,20 @@ package com.example.studentlist2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class EvanGrauActivity extends AppCompatActivity {
 
     ListView languageList;
+    TextView textView;
 
     // array of app ideas
     String appIdeas[] = {
@@ -30,5 +37,22 @@ public class EvanGrauActivity extends AppCompatActivity {
         languageList = (ListView)findViewById(R.id.EKG_ListView);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, appIdeas);
         languageList.setAdapter(arrayAdapter);
+    }
+
+    public void onItemClick(AdapterView<?> adapterView, View view, int position)
+    {
+        // we got notified that something was selected!  AND we know WHICH ITEM was selected!
+
+        // get the value of the string in the specific position of the array that was selected (tapped on)
+        String itemSelected = (String) languageList.getAdapter().getItem(position);
+        
+        /** do something cool based on the exact VALUE (string) of the selected item **/
+        if (itemSelected.equals("Bible app but better"))
+        {
+            // go to a new activity
+            Context context = languageList.getContext();
+            Intent intent = new Intent(context, EKGThirdActivity.class);
+            context.startActivity(intent);
+        }
     }
 }
