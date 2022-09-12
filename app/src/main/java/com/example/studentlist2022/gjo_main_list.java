0ec73370f65d9acac9e6f965e7ba1fe61184ffa6
JetpackWaffle <awesomedude2011gray@gmail.com>
2022-09-12 15:31:48 -0500

    Created new activity files

---
+++ app/src/main/java/com/example/studentlist2022/gjo_main_list.java
@@ -0,0 +1,14 @@
+package com.example.studentlist2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gjo_main_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gjo_main_list);
+    }
+}
