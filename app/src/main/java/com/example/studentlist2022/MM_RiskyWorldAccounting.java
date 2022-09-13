488bf2500edaed31b7439c231ec12337f3c3d116
Marlon Miller <mmillermatute18@gmail.com>
2022-09-12 23:58:37 -0500

    App description

---
+++ app/src/main/java/com/example/studentlist2022/MM_RiskyWorldAccounting.java
@@ -0,0 +1,14 @@
+package com.example.studentlist2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MM_RiskyWorldAccounting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mm_risky_world_accounting);
+    }
+}
--- app/src/main/java/com/example/studentlist2022/MarlonMillerActivity.java
+++ app/src/main/java/com/example/studentlist2022/MarlonMillerActivity.java
@@ -2,11 +2,16 @@ package com.example.studentlist2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class MarlonMillerActivity extends AppCompatActivity {
+public class MarlonMillerActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     ListView languageList;
 
@@ -16,6 +21,11 @@ public class MarlonMillerActivity extends AppCompatActivity {
             "ACU Greek Life",
             "Money Handler",
             "The Social Meme",
+            "CS vs DET",
+            "My Bank",
+            "Fitness App",
+            "Honduras Travel",
+            "ACU Tour"
     };
 
     @Override
@@ -26,5 +36,33 @@ public class MarlonMillerActivity extends AppCompatActivity {
         languageList = (ListView)findViewById(R.id.App_MEMM);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, langArray);
         languageList.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
+    {
+        String appSelected = (String) languageList.getAdapter().getItem(position);
+        /** do something cool based on the INDEX (int) of the selected item **/
+        switch (position)
+        {
+            case 1:
+                // by specific index
+                Toast.makeText(this, appSelected + " app selected", Toast.LENGTH_LONG).show();
+                break;
+
+            default:
+                // every case that wasn't specifically called out
+                Toast.makeText(this, appSelected + " selected", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        //String appSelected = (String) languageList.getAdapter().getItem(position);
+        if (appSelected.equals("Risky World Accounting"))
+        {
+            // go to a new activity
+            Context context = languageList.getContext();
+            Intent intent = new Intent(context, MM_RiskyWorldAccounting.class);
+            context.startActivity(intent);
+        }
     }
 }
