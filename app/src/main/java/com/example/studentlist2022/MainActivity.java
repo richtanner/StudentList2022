eda6953cfeb59e4aa33c2d43a6ec2459939854e4
benjaminbriggs <bbriggs2002@gmail.com>
2022-09-19 17:17:15 -0500

    BriggsBranch, making stuff cool

--- app/src/main/java/com/example/studentlist2022/MainActivity.java
+++ app/src/main/java/com/example/studentlist2022/MainActivity.java
@@ -100,7 +100,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             Intent intent = new Intent(context, ProfTannerActivity.class);
             context.startActivity(intent);
         }
-        if (personSelected.equals("Griffin Barnard, CS"))
+        else if (personSelected.equals("Griffin Barnard, CS"))
         {
             // go to a new activity
             Context context = simpleList.getContext();
@@ -108,23 +108,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             context.startActivity(intent);
         }
 
-        /** do something cool based on the INDEX (int) of the selected item **/
-        switch (position)
-        {
-            case 14:
-                // by specific index
-                Toast.makeText(this, personSelected + " created the Master branch!!", Toast.LENGTH_LONG).show();
-                break;
-
-            default:
-                // every case that wasn't specifically called out
-                Toast.makeText(this, personSelected + " selected", Toast.LENGTH_LONG).show();
-                break;
-        }
-
-
         /** do something cool based on the exact VALUE (string) of the selected item **/
-        if (personSelected.equals("Kelli Norris, DET"))
+        else if (personSelected.equals("Kelli Norris, DET"))
         {
             // go to a new activity
             Context context = simpleList.getContext();
@@ -203,6 +188,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             //Goes to a new activity
             Context context = simpleList.getContext();
             Intent intent = new Intent(context, AlexB_Activity.class);
+            context.startActivity(intent);
+        }
+        else if (personSelected.equals("Ben Briggs, DET"))
+        {
+            // go to my activity
+            Context context = simpleList.getContext();
+            Intent intent = new Intent(context, BriggsAppIdeas.class);
             context.startActivity(intent);
         }
     }
